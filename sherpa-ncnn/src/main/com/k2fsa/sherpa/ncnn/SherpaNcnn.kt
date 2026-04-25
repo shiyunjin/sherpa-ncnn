10d34cfb73e63e6f10a8e77e3de098163ab303b1
shiyunjin <admin@shiyunjin.cn>
2026-04-25 08:16:24 +0800

    feat: java

---
+++ sherpa-ncnn/src/main/com/k2fsa/sherpa/ncnn/SherpaNcnn.kt
@@ -0,0 +1,102 @@
+package com.k2fsa.sherpa.ncnn
+
+import android.content.res.AssetManager
+
+data class FeatureExtractorConfig(
+    var sampleRate: Float,
+    var featureDim: Int,
+)
+
+data class ModelConfig(
+    var encoderParam: String,
+    var encoderBin: String,
+    var decoderParam: String,
+    var decoderBin: String,
+    var joinerParam: String,
+    var joinerBin: String,
+    var tokens: String,
+    var numThreads: Int = 1,
+    var useGPU: Boolean = true,
+)
+
+data class DecoderConfig(
+    var method: String = "modified_beam_search",
+    var numActivePaths: Int = 4,
+)
+
+data class RecognizerConfig(
+    var featConfig: FeatureExtractorConfig,
+    var modelConfig: ModelConfig,
+    var decoderConfig: DecoderConfig,
+    var enableEndpoint: Boolean = true,
+    var rule1MinTrailingSilence: Float = 2.4f,
+    var rule2MinTrailingSilence: Float = 1.0f,
+    var rule3MinUtteranceLength: Float = 30.0f,
+    var hotwordsFile: String = "",
+    var hotwordsScore: Float = 1.5f,
+)
+
+class SherpaNcnn(
+    var config: RecognizerConfig,
+    assetManager: android.content.res.AssetManager? = null,
+) {
+    private val ptr: Long
+
+    init {
+        if (assetManager != null) {
+            ptr = newFromAsset(assetManager, config)
+        } else {
+            ptr = newFromFile(config)
+        }
+    }
+
+    protected fun finalize() {
+        delete(ptr)
+    }
+
+    fun acceptSamples(samples: FloatArray) =
+        acceptWaveform(ptr, samples = samples, sampleRate = config.featConfig.sampleRate)
+
+    fun isReady() = isReady(ptr)
+
+    fun decode() = decode(ptr)
+
+    fun inputFinished() = inputFinished(ptr)
+    fun isEndpoint(): Boolean = isEndpoint(ptr)
+    fun reset(recreate: Boolean = false) = reset(ptr, recreate = recreate)
+
+    val text: String
+        get() = getText(ptr)
+
+    private external fun newFromAsset(
+        assetManager: android.content.res.AssetManager,
+        config: RecognizerConfig,
+    ): Long
+
+    private external fun newFromFile(
+        config: RecognizerConfig,
+    ): Long
+
+    private external fun delete(ptr: Long)
+    private external fun acceptWaveform(ptr: Long, samples: FloatArray, sampleRate: Float)
+    private external fun inputFinished(ptr: Long)
+    private external fun isReady(ptr: Long): Boolean
+    private external fun decode(ptr: Long)
+    private external fun isEndpoint(ptr: Long): Boolean
+    private external fun reset(ptr: Long, recreate: Boolean)
+    private external fun getText(ptr: Long): String
+
+    companion object {
+        var isLibraryLoaded = false
+            private set
+
+        init {
+            try {
+                java.lang.System.loadLibrary("sherpa-ncnn-jni")
+                isLibraryLoaded = true
+            } catch (e: java.lang.UnsatisfiedLinkError) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
